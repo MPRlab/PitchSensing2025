64e63ccc2e81f4f9e658515790938476751cfc3a
G-Chist <42654606+G-Chist@users.noreply.github.com>
2023-03-01 17:53:55 +0800

    Update song_diagram.py

--- song_diagram.py
+++ song_diagram.py
@@ -1,11 +1,43 @@
-import pyaudio
 import wave
 import matplotlib.pyplot as plt
 import numpy as np
-from math import sqrt
+import math
+
+def mean(a):
+    return sum(a)/len(a)
+
+def split_arr(a, ln):
+    #Ищем количество маленьких массивов
+    split_a = [[] for i in range(math.ceil(len(a) / ln))]
+    c = 0
+    for i in a:
+        split_a[c//ln].append(i)
+        c += 1
+    return split_a
+
+def split_mean(a, ln):
+    split_a = split_arr(a, ln)
+    split_mean_arr = []
+    for i in split_a:
+        avg = mean(i)
+        for j in range(ln):
+            if len(split_mean_arr) < len(a):
+                split_mean_arr.append(avg)
+    return split_mean_arr
+
+def split_max(a, ln):
+    split_a = split_arr(a, ln)
+    split_max_arr = []
+    for i in split_a:
+        maxi = max(i)
+        for j in range(ln):
+            if len(split_max_arr) < len(a):
+                split_max_arr.append(maxi)
+    return split_max_arr
 
 #Название файла с песней
-song_file = "C:\\Users\\79140\\Desktop\\audiobred.wav"
+#song_file = "C:\\Users\\79140\\Desktop\\Всячина\\Музыка\\Музыка\\hxh_departure.wav"
+song_file = "C:\\Users\\79140\\Desktop\\Всячина\\Музыка\\Музыка\\Amen break\\amen-break_175bpm.wav"
 
 song = wave.open(song_file, "r")
 raw0 = song.readframes(-1)
@@ -13,26 +45,22 @@ rate = 44100
 symbols_in_sample = 4
 
 #Обрезка фрагмента
-#raw0 = raw0[0:int(rate*symbols_in_sample*1):]
+#raw0 = raw0[0:int(rate*symbols_in_sample*8):]
 
 raw = np.frombuffer(raw0, dtype=np.int32)
 sample_rate = song.getframerate()
-raw = [i for i in raw]
+raw = [abs(i)/100 for i in raw]
 
-#Упрощение звука
-simp_rate = 44100
-raw1 = []
+#Максимумы громкости
+max_arr = split_max(raw, 5000)
 
-for i in range(len(raw) % simp_rate):
-    raw.append(0)
+#Средний максимум
+max_avg = mean(max_arr)
 
-for i in range(0, len(raw)):
-    ind = (i // simp_rate) * simp_rate
-    raw1.append(max(raw[ind:ind+1:]) if raw[i] == max(raw[ind:ind+1:]) else 0)
-
-t = np.linspace(0, len(raw) / sample_rate, num=len(raw))
 plt.title("Waveform")
-plt.plot(t, raw1, color="blue")
+plt.plot(raw, color="blue")
+plt.plot(max_arr, color="red")
+plt.plot([max_avg for i in raw], color="#ff891e")
 plt.ylabel("Amplitude")
-plt.xlabel("Time (s)")
+plt.xlabel("Time")
 plt.show()
